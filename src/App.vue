ab4219f74f9b524d5d448e609a2a3220f9316413
thanasapL <thanasap.lee@outlook.com>
2021-08-20 22:55:26 +0700

    demo navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div class="navbar bg-pink-800 text-neutral-content mb-2">
+      <div class="flex-1 px-2 mx-2">
+        <div class="items-stretch hidden  lg:flex">
+          <router-link to="/" class="btn btn-ghost btn-sm rounded-btn text-xl md:text-lg"> Home </router-link>
+          <a class="btn btn-ghost btn-sm rounded-btn"> Portfolio </a>
+          <a class="btn btn-ghost btn-sm rounded-btn"> About </a>
+          <a class="btn btn-ghost btn-sm rounded-btn"> Contact </a>
+        </div>
+      </div>
+
+      <div class="flex-1 px-2 mx-2">
+        <router-link to="/">
+          <img src="./assets/logo.png" class="w-20" /></router-link>
+      </div>
+
+      <div class="flex-none">
+        <button class="btn btn-square btn-ghost">
+        <i class="material-icons"> shopping_cart </i>
+        </button>
+        <button class="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-6 h-6 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </button>
+        <div class="avatar">
+          <div class="rounded-full w-10 h-10 m-1">
+            <img src="https://i.pravatar.cc/500?img=32" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">
+      <img class="w-16" alt="KaoFood logo" src="../assets/logo.png"/>
+    </router-link>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
