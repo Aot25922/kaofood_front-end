577141ac6128636187d5180b6c78badd392a2e5f
thanasapL <thanasap.lee@outlook.com>
2021-08-23 20:55:00 +0700

    navBar done

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,40 @@
 <template>
   <div id="app">
-    <div class="navbar bg-pink-800 text-neutral-content mb-2">
+    <div class="navbar bg-fire-darker text-neutral-content mb-2">
       <div class="flex-1 px-2 mx-2">
-        <div class="items-stretch hidden  lg:flex">
-          <router-link to="/" class="btn btn-ghost btn-sm rounded-btn text-xl md:text-lg"> Home </router-link>
-          <a class="btn btn-ghost btn-sm rounded-btn"> Portfolio </a>
-          <a class="btn btn-ghost btn-sm rounded-btn"> About </a>
-          <a class="btn btn-ghost btn-sm rounded-btn"> Contact </a>
+        <div class="items-stretch hidden lg:flex">
+          <router-link to="/" class="btn btn-ghost btn-sm rounded-btn text-xl md:text-lg">Home</router-link>
+          <router-link to="" class="btn btn-ghost btn-sm rounded-btn text-xl md:text-lg">Menu</router-link>
+          <router-link to="" class="btn btn-ghost btn-sm rounded-btn text-xl md:text-lg">Contact</router-link>
         </div>
       </div>
 
       <div class="flex-1 px-2 mx-2">
         <router-link to="/">
-          <img src="./assets/logo.png" class="w-20" /></router-link>
+          <img src="./assets/logo.png" class="w-20"
+        /></router-link>
       </div>
 
-      <div class="flex-none">
+      <div class="flex-none mx-5">
         <button class="btn btn-square btn-ghost">
-        <i class="material-icons"> shopping_cart </i>
+          <i class="material-icons"> shopping_cart </i>
         </button>
         <button class="btn btn-square btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            class="inline-block w-6 h-6 stroke-current"
-          >
+            class="inline-block w-6 h-6 stroke-current">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
           </svg>
         </button>
-        <div class="avatar">
-          <div class="rounded-full w-10 h-10 m-1">
-            <img src="https://i.pravatar.cc/500?img=32" />
-          </div>
-        </div>
+        <button class="btn btn-square btn-ghost">
+            <i class="material-icons"> account_circle </i>
+        </button>
       </div>
     </div>
   </div>
@@ -47,9 +43,7 @@
 <script>
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      fire: {
+        lighter: '#F7B267',
+        light: '#F79D65',
+        DEFAULT: '#F4845F',
+        dark: '#F27059',
+        darker: '#F25C54'
+      }
+    },
   },
   variants: {
     extend: {},
