3091af1474b7eaace41fdf6881b2d2f9839d1e8b
nithach2321 <nithach@outlook.co.th>
2021-08-25 19:13:32 +0700

    Move Code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="home">
-    
+  <div id="app">
+    <menu-bar />
+    <intro-concept />
+    <AccountPopup />
+    <Footer />
   </div>
 </template>
 
+<script>
+import MenuBar from '@/components/MenuBar.vue'
+import IntroConcept from '@/components/IntroConcept.vue'
+import Footer from '@/components/Footer.vue'
+import AccountPopup from '@/components/AccountPopup.vue'
+export default {
+  components: { MenuBar, IntroConcept, Footer, AccountPopup},
+};
+</script>
