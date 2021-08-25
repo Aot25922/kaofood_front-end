820252ecd8460eb7009380b98944d97e2dfee647
thanasapL <thanasap.lee@outlook.com>
2021-08-25 23:13:54 +0700

    responsive except menu

--- src/components/IntroConcept.vue
+++ src/components/IntroConcept.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="introConcept">
     <div class="text-center relative text-white">
-      <img src="../assets/header.jpg" alt="Header image" class="filter brightness-50" />
+      <img src="../assets/header.jpg" alt="Header image" class="filter brightness-50 w-full" />
       <div class="font-medium absolute transform inset-0 top-1/4">
-        <p class="text-8xl font-bold uppercase">Kaofood Restaurant</p>
-        <p class="text-3xl my-10">If you're hungry that's your business not us.</p>
-        <p class="text-3xl my-10">We have many menus but we don't have "Anything will do".</p>
-        <button class="mt-5 btn btn-accent btn-lg text-3xl">Let's Eat!</button>
+        <p class="xl:text-8xl text-4xl font-bold uppercase">Kaofood Restaurant</p>
+        <p class="xl:text-3xl text-base xl:my-10 mt-5">If you're hungry that's your business not us. We have many menus but we don't have "Anything will do".</p>
+        <button class="mt-5 btn btn-accent xl:btn-lg btn xl:text-3xl text-xl">Let's Eat!</button>
       </div>
     </div>
   </div>
