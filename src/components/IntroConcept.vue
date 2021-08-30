0ca6ef63749d451d086982e1149e630350a606a1
Aot25922 <aottanapat25922@gmail.com>
2021-08-30 21:33:01 +0700

    Revert "login system v.1.0.0"
    This reverts commit d264e3db465c7b7823d33334a7cfb8a9f1bb4245.

--- src/components/IntroConcept.vue
+++ src/components/IntroConcept.vue
@@ -7,7 +7,6 @@
       <img src="../assets/header.jpg" alt="Header image" class="filter brightness-50 md:w-full w-0" />
       <div class="font-medium absolute transform inset-0 top-1/4">
         <p class="xl:text-8xl md:text-5xl sm:text-4xl text-2xl font-bold uppercase">Kaofood Restaurant</p>
-        <p>{{account}}</p>
         <p class="xl:text-3xl md:text-2xl sm:text-base text-xs xl:my-10 sm:mt-5 mt-1 md:px-5 px-2">If you're hungry that's your business not us. We have many menus but we don't have "Anything will do".</p>
         <button class="mt-5 btn btn-accent md:btn-lg sm:btn-md btn-sm xl:text-3xl md:text-2xl sm:text-lg text-xs">Let's Eat!</button>
       </div>
@@ -18,6 +17,5 @@
 <script>
 export default {
   name: "IntroConcept",
-  inject : ["account"]
 };
 </script>
