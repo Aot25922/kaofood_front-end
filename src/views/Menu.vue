7775b5f02c3d9566ed30ce6d62b7c9d65b7cb0c7
nithach2321 <56868125+nithach2321@users.noreply.github.com>
2021-09-01 18:21:43 +0700

    APP v1.0.1 (Can Get menu-img) (#8)
    * New dev
    
    * npm update
    
    * del test in menu
    
    * Fix cate menu
    
    * Rename to Foodlist
    
    Co-authored-by: thanasapL <thanasap.lee@outlook.com>

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="menu">
-    <category/>
-    <menu-list/>
+    <category @SelectCate="selectCate" />
+    <food-list :cateId="cateId"/>
   </div>
 </template>
 
 <script>
-import Category from '@/components/Category.vue'
-import MenuList from '@/components/MenuList.vue'
+import Category from "@/components/Category.vue";
+import FoodList from "@/components/FoodList.vue";
 export default {
-    name: "Menu",
-    components: { Category, MenuList },
-    methods: {
-    async getMenuList() {
-      try {
-        const res = await fetch(this.menuUrl);
-        const data = res.json();
-        return data;
-      } catch (error) {
-        console.log(`Counld not get! ${error}`);
-      }
-    },
-    selectCate(id){
-      this.cateId = id;
+  name: "Menu",
+  components: { Category, FoodList },
+   data() {
+    return {
+      cateId : null,
+    };
+  },
+  methods: {
+  async getFoodList() {
+    try {
+      const res = await fetch(this.menuUrl);
+      const data = res.json();
+      return data;
+    } catch (error) {
+      console.log(`Counld not get! ${error}`);
     }
   },
-}
+  selectCate(id) {
+    this.cateId = id;
+  },
+},
+};
 </script>
