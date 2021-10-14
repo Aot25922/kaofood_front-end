05a766e826a0aa96d3a3ee6045e61eeaa218a200
nithach2321 <56868125+nithach2321@users.noreply.github.com>
2021-10-14 23:39:45 +0700

    App 0.4.1 (change BackendURL)

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default createStore({
   state: {
-    backendUrl: "https://kaofood.works/api",
+    backendUrl: "http://kaofood.works/api",
     // backendUrl: "http://localhost:8080",
     account: null,
     users: [],
