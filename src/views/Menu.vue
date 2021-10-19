77ea9152a1e6e826ac99138ab6d3bca06b512fb6
thanasapL <thanasap.lee@outlook.com>
2021-10-19 13:56:39 +0700

    pull

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -3,7 +3,7 @@
     <!--FoodList with Loop-->
     <category @SelectCate="selectCate" class="lg:mt-24 md:mt-20 mt-16"/>
     <h1 class="text-center xl:text-3xl lg:text-2xl md:text-xl text-lg xl:py-4 lg:py-3 py-2 font-semibold">Our Menu</h1>
-    <VSPagination class=""/>
+    <!-- <jw-pagination :items="menu" @changePage="onChangePage"></jw-pagination> -->
     <div class="xl:grid-rows-none lg:grid lg:grid-cols-3 lg:grid-rows-3 md:px-6 px-3 md:grid md:grid-cols-2 md:grid-rows-2 sm:flex sm:flex-row">
       <router-link to="/menu/add" v-if="this.$store.state.account!=null && this.$store.state.account.role!='Member'" class="md:m-3 md:px-1">
         <!-- Appear when Phone and hiiden un Ipad -->
@@ -19,20 +19,23 @@
 <script>
 import Category from "@/components/Category.vue";
 import FoodList from "@/components/FoodList.vue";
-import VSPagination from "@/components/VSPagination.vue";
 
 export default {
   name: "Menu",
-  components: {Category, FoodList, VSPagination},
+  components: { Category, FoodList },
   data() {
     return {
       cateId: null,
+      pageOfItems: []
     };
   },
   methods: {
     selectCate(id) {
       this.cateId = id;
     },
+    onChangePage(pageOfItems){
+      this.pageOfItems = pageOfItems;
+    }
   },
   computed: {
     menuFilterList() {
