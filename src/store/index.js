2625b4a6e75596be1c3cced6932e85de3e4ff977
nithach2321 <Nithach@outlook.co.th>
2022-05-14 18:50:59 +0700

    Change backendUrl to env file

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import Swal from 'sweetalert2'
 
 export default createStore({
   state: {
-    backendUrl: "https://bom2321.thddns.net:7997/kaofood",
+    backendUrl: process.env.VUE_APP_Backend_URL + "/kaofood",
     account: null,
     menus: [],
     categories: [],
